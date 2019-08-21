0610d530218f5518010d951486c8ee86025caee0
alexkozopolianski <42234681+alexkozopolianski@users.noreply.github.com>
2019-08-21 12:52:12 +0300

    Add files via upload

---
+++ src/components/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Подтвердить';
+  }
+
+  renderButton(color){
+      return (
+        <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+            {({ language }) => this.renderSubmit(language) }
+        </LanguageContext.Consumer>
+    </button>
+      );
+  }
+    static contextType = LanguageContext;
+    render() {
+        
+        return(
+           <ColorContext.Consumer >
+               { color => this.renderButton(color)}
+           </ColorContext.Consumer>
+        );
+    }
+}
+
+
+export default Button;
