0de2019702606c0913a8c9ae2f9d986742e654fb
alexkozopolianski <42234681+alexkozopolianski@users.noreply.github.com>
2019-08-21 12:50:01 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import UserCreate from './components/UserCreate';
+import { LanguageStore } from './contexts/LanguageContext';
+import ColorContext from './contexts/ColorContext';
+import LanguageSelector from './components/LanguageSelector'
+import './App.css';
+
+class App extends React.Component {
+  
+  render(){
+    return(
+      <div className="ui container">
+       <LanguageStore>
+         <LanguageSelector  />
+           <ColorContext.Provider value="red">
+            <UserCreate />
+           </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    )
+  }
+}
+
+export default App;
