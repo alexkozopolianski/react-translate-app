5f0443720aa8acaa1dbe71cba1ae82dfa312b526
Alex Kozopolianski <42234681+alexkozopolianski@users.noreply.github.com>
2023-10-11 23:13:45 +0300

    Update Button.js to func component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,31 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
-class Button extends React.Component {
-  renderSubmit(language) {
-    return language === 'english' ? 'Submit' : 'Подтвердить';
-  }
+const Button = () => {
+    const language = useContext(LanguageContext);
+    const color = useContext(ColorContext);
 
-  renderButton(color){
-      return (
-        <button className={`ui button ${color}`}>
-        <LanguageContext.Consumer>
-            {({ language }) => this.renderSubmit(language) }
-        </LanguageContext.Consumer>
-    </button>
-      );
-  }
-    static contextType = LanguageContext;
-    render() {
-        
-        return(
-           <ColorContext.Consumer >
-               { color => this.renderButton(color)}
-           </ColorContext.Consumer>
+    const renderSubmit = (language) => {
+        return language === 'english' ? 'Submit' : 'Подтвердить';
+    }
+
+    const renderButton = (color) => {
+        return (
+            <button className={`ui button ${color}`}>
+                {renderSubmit(language)}
+            </button>
         );
     }
-}
 
+    return (
+        <ColorContext.Consumer>
+            {(color) => renderButton(color)}
+        </ColorContext.Consumer>
+    );
+}
 
 export default Button;
