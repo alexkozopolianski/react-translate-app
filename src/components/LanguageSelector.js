e6eba80e09079943f5082bb08381ba36fac19a10
Alex Kozopolianski <42234681+alexkozopolianski@users.noreply.github.com>
2023-10-12 15:47:16 +0300

    Update LanguageSelector.js

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
-class LanguageSelector extends React.Component {
-    static contextType = LanguageContext;
-    render(){
-        return (
-            <div>
-            Select a language:
-            <i className="flag us" onClick={() => this.context.onLanguageChange('english')} />
-            <i className="flag ru"  onClick={() => this.context.onLanguageChange('russian')} />
-          </div>
-        );
-    }
+const LanguageSelector = () => {
+  const { onLanguageChange } = useContext(LanguageContext);
+
+  return (
+    <div>
+      Select a language:
+      <i className="flag us" onClick={() => onLanguageChange('english')} />
+      <i className="flag ru" onClick={() => onLanguageChange('russian')} />
+    </div>
+  );
 }
 
 export default LanguageSelector;
